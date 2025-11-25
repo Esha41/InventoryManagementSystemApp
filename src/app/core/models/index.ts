bc4605f543111884f8501296e628e2b45b8f2e40
Houssem-nawar-flora <nawar.tech@floragroup.net>
2025-11-25 07:55:44 +0300

    Revert "add-email-settings"

--- src/app/core/models/index.ts
+++ src/app/core/models/index.ts
@@ -16,7 +16,6 @@ export * from './ammunition.model';
 export * from './workflow.model';
 export * from './inventory.model';
 export * from './notification.model';
-export * from './email-configuration.model';
 export * from './supply-order.model';
 export * from './supply-request.model';
 export * from './workflow-approval.model';
