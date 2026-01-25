635ba8dd785079dd01016fa67920b17dbba45473
eashaanjum <easha.tech@floragroup.net>
2026-01-25 14:01:03 +0300

    removed unused code

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -17,4 +17,3 @@ export * from './loading-state/loading-state.component';
 export * from './error-state/error-state.component';
 export * from './image-preview-tooltip/image-preview-tooltip.component';
 export * from './request-filter-bar/request-filter-bar.component';
-export * from './report-metadata-dialog/report-metadata-dialog.component';
