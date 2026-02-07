c95350dc97acff633c9914ed4febcf8da0e20909
Easha Anjum <easha.tech@floragroup.net>
2026-02-07 19:28:14 +0300

    revert port numbers

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44369/api',
+  apiUrl: 'https://localhost:7148/api',
   appName: 'Ettad (Dev)',
   enableLogging: false,
   version: '1.0.0',
-  notificationHubUrl: 'https://localhost:44369/hubs/notification'
+  notificationHubUrl: 'https://localhost:7148/hubs/notification'
 };
 
