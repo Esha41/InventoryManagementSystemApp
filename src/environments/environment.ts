fbc7dba3aff43ab3b9027034f4e8643b94263816
eashaanjum <easha.tech@floragroup.net>
2026-03-12 12:29:42 +0300

    fixed validation for past pickupdate

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment: Environment = {
   apiUrl: 'https://localhost:7148/api',
   appName: 'Ettad (Dev)',
   enableLogging: true,
-  version: '1.1.2',
+  version: '1.2.0',
   notificationHubUrl: 'https://localhost:7148/hubs/notification'
 };
 
